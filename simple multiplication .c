2ed85ae0fd0263a0faed72993c2c0a9cde4fa247
mekalamokshaarjunreddy <mekalamokshaarjunreddy@gmail.com>
2025-10-04 11:43:47 +0530

    my first code

---
+++ simple multiplication .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+ int i =1,a;
+ printf("enter a number u want to multiply");
+ scanf("%d",&a);
+ while(i<11){
+   printf("%d\n",a*i);
+   i++;
+   }
+return 0;
+}   
+    
