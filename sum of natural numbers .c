1016f89825c1bd131811566dd19816672f1b45e4
mekalamokshaarjunreddy <mekalamokshaarjunreddy@gmail.com>
2025-10-09 18:14:11 +0530

    Add files via upload

---
+++ sum of natural numbers .c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() 
+{
+int a,sum=0,i=1;
+printf("enter a num");
+scanf("%d",&a);
+while(i<=a){
+ sum =sum +i;
+  i++;
+  }
+printf("%d",sum)  ;
+return 0;
+
+
+
+
+
+
+
+
+
+}
