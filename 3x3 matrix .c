4aaf9f1b1565a836ae9c47d7e23b4d7b2c984ffa
mekalamokshaarjunreddy <mekalamokshaarjunreddy@gmail.com>
2025-11-18 11:11:44 +0530

    Add files via upload

---
+++ 3x3 matrix .c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+int i,a[3][3],j;
+for(i=0;i<3;i++){
+ for(j=0;j<3;j++){
+ 
+scanf("%d",&a[i][j]);}
+}
+for(i=0;i<3;i++){
+ for(j=0;j<3;j++){
+printf("%d\t",a[i][j]);}
+printf(" \n");
+
+
+}
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+    
